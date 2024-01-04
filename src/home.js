d2e698d2d0be24e862539aeff6e3f325ccb53d7f
Ameer <ameerapex496@gmail.com>
2024-01-04 05:50:07 +0100

    added commments + selected elements needed

---
+++ src/home.js
@@ -0,0 +1,12 @@
+//   NAVBAR //
+//
+//
+// selecting elements
+const burgerMenuWrap = document.getElementsByClassName("burger-Menu-Wrap")[0];
+const navRightLinksWrapEl = document.getElementsByClassName(
+  "nav-Right-Links-Wrap"
+)[0];
+
+// functions
+
+// event linsters
