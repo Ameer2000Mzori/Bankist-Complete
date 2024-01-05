9b70dbb517baff285a01f762ba676344e9eb9192
Ameer <ameerapex496@gmail.com>
2024-01-05 07:35:07 +0100

    added all data needed for  object data arry

--- src/home.js
+++ src/home.js
@@ -30,6 +30,18 @@ const operData = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
+  {
+    icon: "fa-solid fa-user",
+    title: "Buy a home or make your dreams come true, with instant loans.    ",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    icon: "fa-solid fa-download",
+    title: "No longer need your account? No problem! Close it instantly.",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
 ];
 
 // functions
