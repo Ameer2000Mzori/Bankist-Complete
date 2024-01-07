23d70f242fe245c282177603610df013e083b4a0
Ameer <ameerapex496@gmail.com>
2024-01-07 07:34:40 +0100

    added logic to get data from input texts

--- src/createAccountLogic.js
+++ src/createAccountLogic.js
@@ -1,13 +1,22 @@
 // selecting elements
-const inputPassword = document.getElementsByClassName("input-Password")[0];
 const inputName = document.getElementsByClassName("input-Name")[0];
 const inputEmail = document.getElementsByClassName("input-Email")[0];
+const inputPassword = document.getElementsByClassName("input-Password")[0];
 const submitCreationAccountBtn = document.getElementsByClassName(
   "submit-Creation-Account"
 )[0];
 
 // gelobal varibales
+const accountsDataObj = [];
 
 // functions
+const createAccount = () => {
+  let userName = inputName.value;
+  let userEmail = inputEmail.value;
+  let userPass = inputPassword.value;
+
+  console.log(userName, userEmail, userPass);
+};
 
 // eventlinsters
+submitCreationAccountBtn.addEventListener("click", createAccount);
