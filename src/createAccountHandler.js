1e69cd730498bac87c6c1351889b5a7f252b5333
Ameer <ameerapex496@gmail.com>
2024-01-07 06:57:47 +0100

    added styling and also selected more elements and small bug fix in styling name

--- src/createAccountHandler.js
+++ src/createAccountHandler.js
@@ -1,7 +1,19 @@
 // selecting elements
+const openAccountsBtns = document.querySelectorAll("#open-Accounts-Btns");
+const accountEl = document.getElementsByClassName("account")[0];
+const accountOverlayEl = document.getElementsByClassName("account-Overlay")[0];
+const accountOverlayClickEl = document.getElementsByClassName(
+  "account-overlay-Click"
+)[0];
 
 // gelobal varibales
 
 // functions
+const openCardHandler = () => {
+  console.log("wow");
+};
 
 // eventlinsters
+openAccountsBtns.forEach((openAccountBtn) => {
+  openAccountBtn.addEventListener("click", openCardHandler);
+});
