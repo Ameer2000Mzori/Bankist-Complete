bd9728827853bb2b9f558a3d2cc03820401e2162
Ameer <ameerapex496@gmail.com>
2024-01-07 07:32:08 +0100

    selected element needed

--- src/createAccountLogic.js
+++ src/createAccountLogic.js
@@ -1,4 +1,10 @@
 // selecting elements
+const inputPassword = document.getElementsByClassName("input-Password")[0];
+const inputName = document.getElementsByClassName("input-Name")[0];
+const inputEmail = document.getElementsByClassName("input-Email")[0];
+const submitCreationAccountBtn = document.getElementsByClassName(
+  "submit-Creation-Account"
+)[0];
 
 // gelobal varibales
 
