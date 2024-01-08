af76444cfd4c2cc7b9584696c0a9230dbdeb1129
Ameer <ameerapex496@gmail.com>
2024-01-08 01:23:36 +0100

    added close login card function and eventlistener

--- src/loginAccountHandler.js
+++ src/loginAccountHandler.js
@@ -1,13 +1,20 @@
 // selecting elements
 const loginBtn = document.getElementById("login-Btn");
 const loginEl = document.getElementsByClassName("login")[0];
-
-// gelobal varibales
+const loginCloseBtn = document.getElementsByClassName("login-Close-Btn")[0];
 
 // functions
+
+// open login card
 const openLoginCard = () => {
   loginEl.classList.add("active");
 };
 
+// close login card
+const closeLoginCard = () => {
+  loginEl.classList.remove("active");
+};
+
 // eventlinsters
 loginBtn.addEventListener("click", openLoginCard);
+loginCloseBtn.addEventListener("click", closeLoginCard);
