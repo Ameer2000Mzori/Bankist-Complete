8a653382a30e8a15084359a200833304c5d06c92
Ameer <ameerapex496@gmail.com>
2024-01-08 01:21:22 +0100

    added openlogin card

--- src/loginAccountHandler.js
+++ src/loginAccountHandler.js
@@ -1,13 +1,13 @@
 // selecting elements
 const loginBtn = document.getElementById("login-Btn");
 const loginEl = document.getElementsByClassName("login")[0];
-const loginEmailInput = document.getElementsByClassName("login-Email-Input")[0];
-const loginPasswordInput = document.getElementsByClassName(
-  "login-Password-Input"
-)[0];
 
 // gelobal varibales
 
 // functions
+const openLoginCard = () => {
+  loginEl.classList.add("active");
+};
 
 // eventlinsters
+loginBtn.addEventListener("click", openLoginCard);
