379e561abec42a818bd0cc319a52c6a6b7342002
Ameer <ameerapex496@gmail.com>
2024-01-08 01:20:16 +0100

    selected elements needed

--- src/loginAccountHandler.js
+++ src/loginAccountHandler.js
@@ -1,4 +1,10 @@
 // selecting elements
+const loginBtn = document.getElementById("login-Btn");
+const loginEl = document.getElementsByClassName("login")[0];
+const loginEmailInput = document.getElementsByClassName("login-Email-Input")[0];
+const loginPasswordInput = document.getElementsByClassName(
+  "login-Password-Input"
+)[0];
 
 // gelobal varibales
 
