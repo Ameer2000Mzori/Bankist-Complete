53cc28a8c78cfde8dda9c4589e02f58a658beb5b
Ameer <ameerapex496@gmail.com>
2024-01-10 00:31:44 +0100

    addd new event linked it to delete account function

--- src/deleteAccountLogic.js
+++ src/deleteAccountLogic.js
@@ -1,22 +1,30 @@
 // imports
+import { accountsDataObj } from "./createAccountLogic.js";
 
 // selecting elements
 
 // selected btn
-const comfirmConfirmPIN = document.getElementsByClassName(
-  "comfirm-Confirm-PIN"
-)[0];
+const comfirmConfirmPIN = document.getElementById("comfirm-Confirm-PIN");
 
 // selected inputs
-const dashboardCardConfirmPIN = document.getElementById(
-  "#dashboard-Card-Confirm-PIN"
-);
 const dashboardCardConfirmUser = document.getElementById(
-  "#dashboard-Card-Confirm-User"
+  "dashboard-Card-Confirm-User"
+);
+
+const dashboardCardConfirmPIN = document.getElementById(
+  "dashboard-Card-Confirm-PIN"
 );
 
 // gelobal varibales
-
+(() => {
+  console.log(accountsDataObj);
+})();
 // functions
 
+// this is delete account function
+const deleteAccount = () => {
+  console.log("account deleted!");
+};
+
 // eventlinsters
+comfirmConfirmPIN.addEventListener("click", deleteAccount);
