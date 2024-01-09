cb3b411d2d329dae3866eaf1dc2e47240d0537d7
Ameer <ameerapex496@gmail.com>
2024-01-10 00:27:36 +0100

    selected inputs + cofirm pin btn

--- src/deleteAccountLogic.js
+++ src/deleteAccountLogic.js
@@ -2,6 +2,19 @@
 
 // selecting elements
 
+// selected btn
+const comfirmConfirmPIN = document.getElementsByClassName(
+  "comfirm-Confirm-PIN"
+)[0];
+
+// selected inputs
+const dashboardCardConfirmPIN = document.getElementById(
+  "#dashboard-Card-Confirm-PIN"
+);
+const dashboardCardConfirmUser = document.getElementById(
+  "#dashboard-Card-Confirm-User"
+);
+
 // gelobal varibales
 
 // functions
