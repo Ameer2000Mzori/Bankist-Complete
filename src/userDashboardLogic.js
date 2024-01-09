09665262b771a3cfa7cf9f31bb9bc4e969f4cfbb
Ameer <ameerapex496@gmail.com>
2024-01-09 03:50:29 +0100

    selected elements for top

--- src/userDashboardLogic.js
+++ src/userDashboardLogic.js
@@ -1,5 +1,20 @@
 // selecting elements
 
+// selecting pages :
+const landingPageEl = document.getElementsByClassName("landing-Page")[0];
+const dashboardPageEl = document.getElementsByClassName("dashboard-Page")[0];
+
+// selecting titles :
+const dashboardTopLeftText1 = document.getElementsByClassName(
+  "dashboard-Top-Left-Text-1"
+)[0];
+const dashboardTopMidText2 = document.getElementsByClassName(
+  "dashboard-Top-Mid-Text-2"
+)[0];
+const dashboardTopRightText1 = document.getElementsByClassName(
+  "dashboard-Top-Right-Text-1"
+)[0];
+
 // data object
 
 // functions
