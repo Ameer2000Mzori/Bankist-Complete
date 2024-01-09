4141b1dbe1b2e3eae102e5af30738504fa552995
Ameer <ameerapex496@gmail.com>
2024-01-09 06:28:06 +0100

    bug fix in timeAndGreet function

--- src/timeAndGreet.js
+++ src/timeAndGreet.js
@@ -6,7 +6,25 @@ const dashboardTopMidText2 = document.getElementsByClassName(
 )[0];
 
 // this is time and greet func
-export const timeAndGreet = (userInfoObject) => {
-  dashboardTopLeftText1.textContent = userInfoObject.userName;
-  dashboardTopMidText2.textContent = `this is time text`;
+export const timeAndGreet = () => {
+  let time = new Date();
+  let year = time.getFullYear();
+  let month = time.getMonth() + 1;
+  let day = time.getDate();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+
+  let timeInterval = setInterval(() => {
+    console.log(year, month, day, hour, min);
+  }, 1000);
+
+  // setting the 0 before each number that is lower then 10
+  month < 10 ? (month = `0${month}`) : month;
+  day < 10 ? (day = `0${day}`) : day;
+  hour < 10 ? (hour = `0${hour}`) : hour;
+  min < 10 ? (min = `0${min}`) : min;
+
+  console.log(year, month, day, hour, min);
+  //   dashboardTopLeftText1.textContent = userInfoObject.userName;
+  dashboardTopMidText2.textContent = `As of ${day}/${month}/${year}, ${hour}:${min}`;
 };
