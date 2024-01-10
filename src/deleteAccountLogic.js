c98a6e7d6d1bb4899dc06061d73b94ffe5544677
Ameer <ameerapex496@gmail.com>
2024-01-10 03:00:03 +0100

    added more logic to delete user function as if user puts wrong password if his account or email he cant delete account

--- src/deleteAccountLogic.js
+++ src/deleteAccountLogic.js
@@ -23,19 +23,20 @@ const dashboardCardConfirmPIN = document.getElementById(
 
 // this is delete account function
 export const deleteAccount = (userInfoObject, checkStatus) => {
-  if (checkStatus) {
-    accountLogout();
-    let userEmailInput = dashboardCardConfirmUser.value;
-    let userPassInput = dashboardCardConfirmPIN.value;
-    console.log("this is userEmail input", userEmailInput);
-    console.log("this is userPass input", userPassInput);
+  // this is user input
+  let userEmailInput = dashboardCardConfirmUser.value;
+  let userPassInput = dashboardCardConfirmPIN.value;
 
-    let indexToRemove = -1;
+  // if user has put his right information or not!
+  if (
+    userInfoObject.userEmail === userEmailInput &&
+    userInfoObject.userPass === userPassInput
+  ) {
+    if (checkStatus) {
+      accountLogout();
 
-    if (
-      userInfoObject.userEmail === userEmailInput &&
-      userInfoObject.userPass === userEmailInput
-    ) {
+      let indexToRemove = -1;
+
       accountsDataObj.forEach((user, index) => {
         if (
           user.userEmail === userEmailInput &&
@@ -52,15 +53,15 @@ export const deleteAccount = (userInfoObject, checkStatus) => {
       console.log("account deleted!");
       console.log("our user data after deletion: ", accountsDataObj);
       console.log("this is user data inside of true", userInfoObject);
-    } else {
-      console.log("your email or password is worng");
+      userInfoObject = "";
     }
   } else {
-    console.log("this is user data inside of false", userInfoObject);
+    console.log("password or email wrong!");
   }
-};
 
-// delete user envent
-comfirmConfirmPIN.addEventListener("click", () => {
-  deleteAccount(true, true);
-});
+  // delete user envent button
+  comfirmConfirmPIN.addEventListener("click", () => {
+    deleteAccount(userInfoObject, true);
+    userInfoObject = "";
+  });
+};
