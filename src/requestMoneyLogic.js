13977b199f1c40b801174db7cecc11a8af807f9f
Ameer <ameerapex496@gmail.com>
2024-01-10 05:36:28 +0100

    added checkUserInput

--- src/requestMoneyLogic.js
+++ src/requestMoneyLogic.js
@@ -6,6 +6,7 @@ const dashboardCardInputRequestLoan = document.getElementById(
 // gelobal varibales
 let userInfo;
 let moneyRequested;
+let stringWithoutDecimal;
 
 // functions
 export const getUserInformation = (userInfoObject) => {
@@ -14,15 +15,24 @@ export const getUserInformation = (userInfoObject) => {
 
 // request money
 const requestMoney = () => {
-  console.log(userInfo);
+  // getting data
+  moneyRequested = dashboardCardInputRequestLoan.value;
+  checkUserInput(stringWithoutDecimal);
+  console.log(`after returning money  :`, stringWithoutDecimal);
+};
+
+const checkUserInput = () => {
+  stringWithoutDecimal = moneyRequested.replace(/\./g, "");
+  if (stringWithoutDecimal === "") {
+    stringWithoutDecimal = 50;
+    dashboardCardInputRequestLoan.value = 50;
+  }
+  if (stringWithoutDecimal >= 1000 || stringWithoutDecimal < 0) {
+    stringWithoutDecimal = 50;
+    dashboardCardInputRequestLoan.value = 50;
+  }
+  dashboardCardInputRequestLoan.value = stringWithoutDecimal;
+  return stringWithoutDecimal;
 };
 // event lisnters
 requestLoanBtn.addEventListener("click", requestMoney);
-dashboardCardInputRequestLoan.addEventListener("input", (e) => {
-  let moneyRequested = e.data;
-
-  if (moneyRequested.includes(".")) {
-    moneyRequested = moneyRequested;
-  }
-  console.log(moneyRequested);
-});
