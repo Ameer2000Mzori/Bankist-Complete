696cd9d2648161867d25c6ec5ac6f93fb2c7d0ce
Ameer <ameerapex496@gmail.com>
2024-01-10 04:49:46 +0100

    added more logic as the user can request money

--- src/requestMoneyLogic.js
+++ src/requestMoneyLogic.js
@@ -1,10 +1,14 @@
 // selected btn // selected inputs
 const requestLoanBtn = document.getElementById("request-Loan-Btn");
-
+const dashboardCardInputRequestLoan = document.getElementById(
+  "dashboard-Card-Input-Request-Loan"
+);
 // gelobal varibales
 let userInfo;
+let moneyRequested;
+
 // functions
-export const getUserInformation = () => {
+export const getUserInformation = (userInfoObject) => {
   userInfo = userInfoObject;
 };
 
@@ -14,3 +18,11 @@ const requestMoney = () => {
 };
 // event lisnters
 requestLoanBtn.addEventListener("click", requestMoney);
+dashboardCardInputRequestLoan.addEventListener("input", (e) => {
+  let moneyRequested = e.data;
+
+  if (moneyRequested.includes(".")) {
+    moneyRequested = moneyRequested;
+  }
+  console.log(moneyRequested);
+});
