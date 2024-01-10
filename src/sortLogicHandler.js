b73c2d1ed29f04ce2d1ea325b05598b07e33a492
Ameer <ameerapex496@gmail.com>
2024-01-10 08:29:35 +0100

    created sort transactions logic for cleaner code

--- src/sortLogicHandler.js
+++ src/sortLogicHandler.js
@@ -27,24 +27,32 @@ const sortTransactions = () => {
     return user.userName === ourUserData.userName;
   });
 
+  sortTransactionsLogic(sortedInfo);
+
   console.log("founded user:", userFound);
   console.log("users accounts ", accountsDataObj);
+};
+
+// sortTransactionsLogic
+const sortTransactionsLogic = () => {
   if (sortStage === 0) {
     sortStage++;
     console.log("sort stage:", sortStage);
     sortedInfo.sort((a, b) => b.transaction - a.transaction);
     console.log("Sorted data:", sortedInfo);
+    return sortedInfo;
   } else if (sortStage === 1) {
     sortStage++;
     console.log("sort stage:", sortStage);
     sortedInfo.sort((a, b) => a.transaction - b.transaction);
     console.log("Sorted data:", sortedInfo);
+    return sortedInfo;
   } else {
     sortStage = 0;
     console.log("sort stage:", sortStage);
     console.log("our normal data:", ourUserData.date);
+    return ourUserData.date;
   }
 };
-
 //events
 dashboardBottomLeftSortBtn.addEventListener("click", sortTransactions);
